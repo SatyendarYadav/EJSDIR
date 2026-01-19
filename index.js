9801c9630df12a139014b389631792b81de90ec4
satyendar <satyendaryadav@hotmail.com>
2026-01-20 01:26:37 +0530

    ejs concept

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const path = require("path");
 
 let port = 8055;
 
+app.use(express.static(path.join(__dirname, "public/js")));
+app.use(express.static(path.join(__dirname, "public/css")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+});
+
+app.get("/rolldice", (req, res) => {
+    let rolldice = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice",{rolldice});
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if (data) {
+        res.render("instagram.ejs", { data});   
+    } else {
+        res.render("error.ejs");
+    }
 });
 
 app.get("/hello", (req, res) => {
